2cf6102e449d77ae3ce71248fd2e70559b31cc80
Taichi Fukawa <ta1fukawa@gmail.com>
2022-04-17 13:50:44 +0000

    Update

---
+++ db/create_table.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    `id`         int          NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255) NOT NULL,
+    `email`      varchar(255) NOT NULL,
+    `created_at` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `removed_at` datetime,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user_profile` (
+    `user_id`    int      NOT NULL,
+    `birthday`   date,
+    `grade`      varchar(255),
+    `position`   varchar(255),
+    `phone`      varchar(255),
+    `address`    varchar(255),
+    `twitter`    varchar(255),
+    `facebook`   varchar(255),
+    `instagram`  varchar(255),
+    `linkedin`   varchar(255),
+    `github`     varchar(255),
+    `website`    varchar(255),
+    `about`      text,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tcu_account` (
+    `username`      varchar(255) NOT NULL,
+    `hash_password` varchar(255) NOT NULL,
+    `mail`          varchar(255) NOT NULL,
+    `usergroup`     varchar(255) NOT NULL,
+    `name_kanji`    varchar(255) NOT NULL,
+    `name_kana`     varchar(255) NOT NULL,
+    `created_at`    datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE iF NOT EXISTS `user_tcu_account` (
+    `user_id`      int          NOT NULL,
+    `username`     varchar(255) NOT NULL,
+    `password`     varchar(255) NOT NULL,
+    `created_at`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`username`) REFERENCES `tcu_account` (`username`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
